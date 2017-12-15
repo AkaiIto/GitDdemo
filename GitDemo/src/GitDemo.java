fa0621bb8e9705185fc3b7c6db804eb1f39bc1c3
Jagdale.Vishal <Jagdale.Vishal@SBR-TECH-040.BANCTECINDIA.IN>
2017-12-15 17:44:49 +0530

    cgangesd

--- GitDemo/src/GitDemo.java
+++ GitDemo/src/GitDemo.java
@@ -3,11 +3,9 @@ public class GitDemo {
 
 	public static void main(String args[])
 	{
-<<<<<<< HEAD
+
 		System.out.println("Hello SVN");
 		System.out.println("yo");
-=======
-		System.out.println("Hello Git is awesome");
->>>>>>> branch 'master' of https://github.com/AkaiIto/GitDdemo.git
+
 	}
 }
